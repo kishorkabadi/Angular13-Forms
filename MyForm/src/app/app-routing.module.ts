f9f15dcdfd530b90e902d3e3f480ec2e408551b9
Kishor Kabadi <kishor.kabadi@intelliquity.in>
2023-07-18 12:10:48 +0530

    Form Validation

--- MyForm/src/app/app-routing.module.ts
+++ MyForm/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:AppComponent
+  },
+  {
+    path:'TemplateForms',component:ReactiveFormsComponent
+  },
+  {
+    path:'ReactiveForms',component:ReactiveFormsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MyForm/src/app/app.component.ts
+++ MyForm/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms'; 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MyForm';
+  ngOnInit()
+  {
+
+
+
+  }
+  Register(regForm:any)
+  {
+console.log(regForm.value);
+console.log(regForm.controls.firstName.value);
+  }
 }
