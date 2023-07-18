b397cfe00689f1372fd74143ea8bf1a4d3d3fc07
Kishor Kabadi <kishor.kabadi@intelliquity.in>
2023-07-18 12:29:21 +0530

    Form Validation

--- MyForm/src/app/reactive-form-with-validators/reactive-form-with-validators.component.ts
+++ MyForm/src/app/reactive-form-with-validators/reactive-form-with-validators.component.ts
@@ -10,22 +10,22 @@ export class ReactiveFormWithValidatorsComponent {
   title = 'Reactive Driven Form with Validator';
   formDetails: FormGroup;
 //Syntax of validator in Angular 13 version
-  // constructor(private frmBuilder: FormBuilder) {
-  //   this.formDetails = frmBuilder.group({
-  //     firstName: ['',Validators.required,Validators.minLength(6)],
-  //     lastName:  ['',Validators.required,Validators.minLength(6)],
-  //     email:  ['',Validators.required,Validators.email]
-  //   })
-  // }
-
-  // Syntax of validator in Angular 16 version
   constructor(private frmBuilder: FormBuilder) {
     this.formDetails = frmBuilder.group({
-      firstName:new FormControl('', [Validators.required,Validators.minLength(6)]),
-      lastName: new FormControl('',[Validators.required,Validators.minLength(6)]),
-      email: new FormControl('',[Validators.required,Validators.email])
+      firstName: ['',[Validators.required,Validators.minLength(6)]],
+      lastName:  ['',[Validators.required,Validators.minLength(6)]],
+      email:  ['',[Validators.required,Validators.email]]
     })
   }
+
+  // Syntax of validator in Angular 16 version
+  // constructor(private frmBuilder: FormBuilder) {
+  //   this.formDetails = frmBuilder.group({
+  //     firstName:new FormControl('', [Validators.required,Validators.minLength(6)]),
+  //     lastName: new FormControl('',[Validators.required,Validators.minLength(6)]),
+  //     email: new FormControl('',[Validators.required,Validators.email])
+  //   })
+  // }
   ngOnInit()
   {}
   submitFormData(regForm:any)
